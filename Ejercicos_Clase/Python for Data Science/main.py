f814f610fea6e81fea6d94043b08efa4b3cd6a04
ojsc <ojsilvera@gmail.com>
2026-03-16 08:22:19 -0500

    ejemplos del uso de type, expresiones, variables y strings o cadenas

--- Ejercicos_Clase/Python for Data Science/main.py
+++ Ejercicos_Clase/Python for Data Science/main.py
@@ -1,2 +1,29 @@
 # Este es un programa simple en Python que imprime "Hello, World!" en la consola.
 print('Hello, World!')
+
+# Esto imprimirá <class 'float'>, indicando que 3.14 es un número de punto flotante.
+print(type(3.14))
+
+# Esto imprimirá <class 'str'>, indicando que 'Hello, World!' es una cadena de texto.
+print(type('Hello, World!'))
+
+# expresión matemática simple que suma dos números.
+result = 5 + 3
+print(result)  # Esto imprimirá 8, que es la suma de 5 y 3.
+
+# variables y asignación
+x = 10
+y = 20
+z = x + y
+print(z)  # Esto imprimirá 30, que es la suma de x e y
+
+# strings y concatenación
+# Esto imprimirá 'John Doe', que es la concatenación de
+# first_name y last_name con un espacio entre ellos.
+
+first_name = 'John'
+last_name = 'Doe'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+
