b35363f925be86f0611b33ed47269d4756ef1027
ojsc <ojsilvera@gmail.com>
2026-03-16 14:45:29 -0500

    Conditions and Branching + quiz terminado

---
+++ Ejercicos_Clase/Python for Data Science/modulo_3/modulo_3.py
@@ -0,0 +1,66 @@
+# Conditions and Branching
+# If, Elif, Else
+# If condition:
+#     # code to execute if condition is true
+# Elif another_condition:
+#     # code to execute if another_condition is true
+# Else:
+#     # code to execute if all conditions are false
+
+# Example:
+x = 10
+if x > 0:
+    print("x is positive")
+elif x < 0:
+    print("x is negative")
+else:    print("x is zero")
+
+# Nested If
+# If condition:
+#     If another_condition:
+#         # code to execute if both conditions are true
+#     Else:
+#         # code to execute if condition is true but another_condition is false
+# Else:
+#     # code to execute if condition is false
+
+# Example:
+y = 5
+if y > 0:
+    if y % 2 == 0:
+        print("y is a positive even number")
+    else:
+        print("y is a positive odd number")
+else:    print("y is not positive")
+
+# quiz
+
+# Select the values of i that produces a True for the following: valor = i!=0
+
+# 1
+# 0
+# -1
+#100000000
+
+# The values of i that produce a True for the condition valor = i != 0 are:
+- 1
+- -1
+- 100000000
+
+# What is the output of the following:
+
+x='a'
+
+if(x!='a'):
+
+    print("This is not a.")
+
+else:
+
+    print("This is a.")
+
+
+# "This is not a."
+# "This is a."
+
+# The output of the code is: "This is a."
