fbffe92ab53e6ed1eed8fa4d02fb4ebc98b48f55
ojsc <ojsilvera@gmail.com>
2026-03-16 20:22:55 -0500

    quiz final

--- Ejercicos_Clase/Python for Data Science/modulo_5/modulo_5.py
+++ Ejercicos_Clase/Python for Data Science/modulo_5/modulo_5.py
@@ -132,5 +132,53 @@ class DatabaseAPI:
 db_api = DatabaseAPI("mi_conexion_a_la_base_de_datos")
 resultados = db_api.query("SELECT * FROM usuarios")
 print(resultados)
-# En este ejemplo, la clase DatabaseAPI proporciona una API simple para interactuar con una base de datos. El método query permite a los desarrolladores ejecutar consultas SQL de manera sencilla, sin tener que preocuparse por los detalles de la conexión o la ejecución de la consulta.
+# En este ejemplo, la clase DatabaseAPI proporciona una API simple para interactuar
+# con una base de datos. El método query permite a los desarrolladores ejecutar
+# consultas SQL de manera sencilla, sin tener que preocuparse por los detalles de
+# la conexión o la ejecución de la consulta.
 
+# quiz
+
+# What is the result of the following lines of code:
+
+a=np.array([0,1,0,1,0])
+b=np.array([1,0,1,0,1])
+a*b
+
+# 0
+# array([1, 1, 1, 1, 1])
+# array([0, 0, 0, 0, 0])
+
+# la respuesta correcta es: array([0, 0, 0, 0, 0])
+
+# What is the result of the following lines of code:
+
+a=np.array([0,1])
+b=np.array([1,0])
+np.dot(a,b)
+
+# 1
+# array([1,1])
+# 0
+# array([0,0])
+
+# la respuesta correcta es: 0
+
+# What is the result of the following lines of code:
+
+a=np.array([1,1,1,1,1])
+a+10
+
+# array([10,10,10,10,10])
+# array([11, 11, 11, 11, 11])
+# array([1,1,1,1,1])
+
+# la respuesta correcta es: array([11, 11, 11, 11, 11])
+
+# What is the correct code to perform matrix multiplication on the matrix A and B?
+
+# np.dot(A,B)
+# A*B
+# AxB
+
+# la respuesta correcta es: np.dot(A,B)
